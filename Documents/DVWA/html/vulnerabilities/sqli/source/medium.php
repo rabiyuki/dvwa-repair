dbe05948fb01e10ed705f408e99eafa9325b8e76
rabiyuki <47032473+rabiyuki@users.noreply.github.com>
2023-03-19 08:04:19 +0900

    SQLインジェクションを対策

--- Documents/DVWA/html/vulnerabilities/sqli/source/medium.php
+++ Documents/DVWA/html/vulnerabilities/sqli/source/medium.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 if( isset( $_POST[ 'Submit' ] ) ) {
 	// Get input
 	$id = $_POST[ 'id' ];
@@ -23,6 +23,51 @@ if( isset( $_POST[ 'Submit' ] ) ) {
 
 // This is used later on in the index.php page
 // Setting it here so we can close the database connection in here like in the rest of the source scripts
+$query  = "SELECT COUNT(*) FROM users;";
+$result = mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '</pre>' );
+$number_of_rows = mysqli_fetch_row( $result )[0];
+
+mysqli_close($GLOBALS["___mysqli_ston"]);
+*/
+
+//以下修正済み
+if( isset( $_POST[ 'Submit' ] ) ) {
+	// Get input
+	$id = $_POST[ 'id' ];
+	$count = 0;
+
+	if ( ctype_digit($id) === false ) {
+		$html .= "<pre>IDが不正な形式です。</pre>";
+		goto db_index;
+	}
+
+	try{
+		$pdo = new PDO('mysql:dbname=dvwa;host=127.0.0.1', 'app', 'vulnerables');
+		$stmt = $pdo->prepare('SELECT first_name, last_name FROM users WHERE user_id = :id');
+		$stmt->bindValue(':id', $id, PDO::PARAM_INT);
+		$stmt->execute();
+		$result = $stmt->fetchAll();
+		$pdo = null;
+		$stmt = null;
+	}catch(PDOException $e){
+		echo "データベースエラー";
+		die();
+	}
+
+	// Get results
+	foreach( $result as $value ) {
+		$first = $value[0];
+		$last  = $value[1];
+
+		// Feedback for end user
+		$html .= "<pre>ID: {$id}<br />First name: {$first}<br />Surname: {$last}</pre>";
+	}
+
+}
+
+// This is used later on in the index.php page
+// Setting it here so we can close the database connection in here like in the rest of the source scripts
+db_index:
 $query  = "SELECT COUNT(*) FROM users;";
 $result = mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '</pre>' );
 $number_of_rows = mysqli_fetch_row( $result )[0];
